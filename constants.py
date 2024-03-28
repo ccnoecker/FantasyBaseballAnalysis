363ded3da0baf3db97e661d5905cb42798ea0021
Curtis Noecker <curtis.noecker@newroom-media.de>
2024-03-28 22:37:50 +0100

    Add constants, utils, data directory

---
+++ constants.py
@@ -0,0 +1,11 @@
+DATA_OUTPUT_PATH = './data'
+
+HITTER_FACTORS_MAPPING = {
+    'Right': 'RHB',
+    'Left': 'LHB',
+    'Switch': 'All'
+}
+PARK_FACTORS_FILENAME = 'park_factors.csv'
+PITCHER_POSITIONS = ['SP', 'RP']
+
+TEAM_FILENAME = 'team.csv'
