6e5753f140daa04b616e60c7ae2f27e33826dd2f
Curtis Noecker <curtis.noecker@newroom-media.de>
2024-04-02 22:20:13 +0200

    Add filenames to constants

--- constants.py
+++ constants.py
@@ -5,7 +5,9 @@ HITTER_FACTORS_MAPPING = {
     'Left': 'LHB',
     'Switch': 'All'
 }
-PARK_FACTORS_FILENAME = 'park-factors.csv'
 PITCHER_POSITIONS = ['SP', 'RP']
 
+ALL_PLAYERS_FILENAME = 'all-players.csv'
+OWNED_PLAYERS_FILENAME = 'owned-players.csv'
+PARK_FACTORS_FILENAME = 'park-factors.csv'
 TEAM_FILENAME = 'team.csv'
