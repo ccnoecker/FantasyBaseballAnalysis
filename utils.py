0a0e3db17c501cf7d485d627cee0e40049dd0627
ccnoecker <ccnoecker@gmail.com>
2024-05-25 20:47:37 +0200

    Get export of available players (#2)
    * Remove unused import
    
    * Add script to scrape list of all players from fantasypros
    
    * Reorder code
    
    * Start working on available players
    
    * Add filenames to constants
    
    * Finalize and rename script to get IDs for PL players
    
    * Update README
    
    * Add utils to clean pl export and get a team id
    
    * Add constants for MLB ID mapping
    
    * Add script to get all players from MLB
    
    * Optimize playerparkfactors to reuse other code and ignore non-standard statdiums
    
    * Switch filename for scraped fantasypros players
    
    * Load all players from MLB API
    
    * Refactor player ID mapping to use direct API request
    
    * Save list of all players
    
    * Remove comment that's no longer accurate
    
    * Add MLB API constants used by PL player IDs
    
    * Lookup MLB IDs when scraping fantasypros rankings
    
    * Add function specifically to search for a player and return the first response
    
    * Join all output files to one master file
    
    * Include player ID in getallplayers

--- utils.py
+++ utils.py
@@ -1,15 +1,49 @@
+from re import findall
+from os import mkdir, path
+
 import pandas as pd
 import statsapi
-from os import mkdir, path
+from requests import get
 
 import constants
 
 
-def lookup_team_id(team_name):
-    lookup_results = statsapi.lookup_team(team_name)
-    if len(lookup_results) != 1:
-        raise ValueError(f'Did not receive one result from statsapi for team name {team_name}: {lookup_results}')
-    return lookup_results[0]['id']
+def clean_pl_export(pl_df:pd.DataFrame):
+    # Remove empty rows and columns
+    pl_df.dropna(how='all', inplace=True)
+    pl_df.dropna(how='all', axis=1, inplace=True)
+
+    # Promote the first row to headers
+    new_header = pl_df.iloc[0] # grab the first row for the header
+    pl_df = pl_df[1:] # take the data less the header row
+    pl_df.columns = new_header # set the header row as the df header
+
+    # Remove columns with irrelevant text and name the first column
+    column_numbers = [x for x in range(pl_df.shape[1])][1:-3]  # list of columns' integer indices
+    pl_df = pl_df.iloc[:, column_numbers] # return selected columns
+    pl_df.rename(columns={pl_df.columns[0]: 'Team Position'}, inplace=True)
+
+    # Remove bench coaches
+    pl_df = pl_df.loc[pl_df['Team Position'] != 'BC']
+    return pl_df
+
+
+# TODO: Optimize to just get a list of all teams rather than requesting for every player
+def get_team_id(team_search_value):
+    team_search_value = team_search_value.upper()
+    if team_search_value.upper() in constants.TEAM_LOOKUP_VALUES.keys():
+        team_search_value = constants.TEAM_LOOKUP_VALUES[team_search_value]
+    lookup_results = statsapi.lookup_team(team_search_value)
+    if len(lookup_results) == 1:
+        return lookup_results[0]['id']
+    elif len(lookup_results) == 0:
+        raise ValueError(f'Got no results from API for team search value {team_search_value}')
+    else:
+        raise ValueError(f'Got more than 1 result from API for team search value {team_search_value}: {lookup_results}')
+
+
+def load_dataframe(filename) -> pd.DataFrame:
+    return pd.read_csv(path.join(constants.DATA_OUTPUT_PATH, filename))
 
 
 def lookup_player_stats(player_name):
@@ -21,11 +55,12 @@ def lookup_player_stats(player_name):
     return player_stats
 
 
-def load_dataframe(filename) -> pd.DataFrame:
-    return pd.read_csv(path.join(constants.DATA_OUTPUT_PATH, filename))
-
-
 def save_dataframe(dataframe, filename):
     if not path.exists(constants.DATA_OUTPUT_PATH):
         mkdir(constants.DATA_OUTPUT_PATH)
     dataframe.to_csv(path.join(constants.DATA_OUTPUT_PATH, filename), index=False)
+
+
+def search_for_player(search_value):
+    lookup_results = get(f'{constants.STATSAPI_PEOPLE_SEARCH_PATH}?names={search_value}&hydrate=currentTeam', timeout=30).json()['people']
+    return next((player for player in lookup_results), None)
